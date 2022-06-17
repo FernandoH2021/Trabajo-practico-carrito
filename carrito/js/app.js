c4da8472b2e9fa48830397d98e13636c7574a580
Fernando <fh1273509@gmail.com>
2022-06-17 13:12:31 -0300

    declaro constantes en js

---
+++ carrito/js/app.js
@@ -0,0 +1,8 @@
+const carrito = document.getElementById("carrito");
+const lista = document.getElementById("lista-carrito");
+const listaAccesorios = document.querySelector("lista-carrito tbody");
+const vaciarCarrito = document.getElementById("vaciar -carrito");
+
+
+
+
